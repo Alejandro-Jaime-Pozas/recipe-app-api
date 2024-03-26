37a3c81fee5f695d77d987fb906434aeb567cf50
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-03-25 22:49:16 -0600

    testing bad credentials dont return a token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -92,3 +92,24 @@ class PublicUserApiTests(TestCase):
         self.assertIn('token', res.data)
         # check that status code is 200 success
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test returns error if credentials invalid."""
+        create_user(email='test@example.com', password='goodpass123')
+
+        payload = {
+            'email': 'test@example.com',
+            'password': 'badpass123',
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_pwd(self):
+        """Test posting a blank password returns an error."""
+        payload = {'email': 'test@example.com', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
