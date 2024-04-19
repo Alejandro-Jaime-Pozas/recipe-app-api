eef9e62bb1ceac0bb3d3161ba96108c8e9c5ceaa
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-04-19 12:00:58 -0600

    added perform_create to specify user for new recipes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -22,9 +22,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')  # self.request trails back to parent View class's request object. request contains headers, query params, data (request body), and user if auth is required
 
+    # manage the serializer returned based on the request
     def get_serializer_class(self):
         """Return the serializer class for request."""
         # if the action is a list action (not detail) then return a list, else detail
         if self.action == 'list':
             return serializers.RecipeSerializer  # return reference to class, not an instance
         return self.serializer_class
+
+    # use builtin perform_create to modify how django saves a serializer/model (should only apply to POST method)
+    def perform_create(self, serializer):  # serializer is already validated prior to this fn call
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)  # this to save the active user as the specified recipe user FK
