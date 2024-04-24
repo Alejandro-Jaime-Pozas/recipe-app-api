8740d7d6e385a7bd025443094f1ade71b667014a
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-04-24 15:38:40 -0600

    added tests bw ingredients within recipes

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -27,6 +27,7 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
     tags = TagSerializer(many=True, required=False)  # set up a field to TagSerializer with multiple available, but no tags are required (field can be blank)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
@@ -37,10 +38,12 @@ class RecipeSerializer(serializers.ModelSerializer):
             'price',
             'link',
             'tags',  # also add tags field set up above to fields here
+            'ingredients',  # also add ingredients field set up above to fields here
         ]
         read_only_fields = ['id']  # api user should not be able to modify recipe id, only other fields
 
 
+    # using '_' at fn start signifies internal use
     def _get_or_create_tags(self, tags, recipe):
         """Handle getting or creating tags as needed."""
         auth_user = self.context['request'].user  # context is a serializer variable passed in from its view
@@ -51,12 +54,24 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Handle getting or creating ingredients as needed."""
+        auth_user = self.context['request'].user  # context is a serializer variable passed in from its view
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(  # get or create gets or creates an object
+                user=auth_user,  # include the user that is trying to access their ingredients
+                **ingredient,  # input all of the tag kwargs required (may update tag reqs which is why **tag helps)
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     # override the create() fn for ModelSerializer in order to create recipe serializer as well as its tags serializers separately
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', [])  # remove the 'tags' field from validated_data dict and store it, default to [] if no data
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)  # create the recipe with updated validated_data (no tags in data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
