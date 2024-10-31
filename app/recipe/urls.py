81ed6c14d03c9c4e991eab3b7b18b63e7e48273d
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-10-30 20:26:55 -0600

    changed the app_name from bottom to top

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -10,13 +10,14 @@ from rest_framework.routers import DefaultRouter
 
 from recipe import views
 
+# used to identify this module when using the include, reverse fns elsewhere
+app_name = 'recipe'
 
 router = DefaultRouter()  # use to automatically determine url config
 router.register('recipes', views.RecipeViewSet)  # this will create auto-generated url endpoints based on the functionality of the view (get, post, put, patch, delete)
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
 
-app_name = 'recipe'  # used to identify this module when using the include, reverse fns elsewhere
 
 urlpatterns = [
     path('', include(router.urls)),  # router.urls is where the different auto-generated urls are contained
